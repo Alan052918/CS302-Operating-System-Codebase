9f745b3d37aeea23246371e29c5dc1dd845e640a
Alan Ai <aijunda29@gmail.com>
2020-03-05 01:23:26 +0800

    Add lab 2 assignment code

---
+++ CS302-Lab02/test_case/lab1-11711310.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+dirc=0
+filc=0
+output=file.info
+
+walk() {
+    printf "[%s]\n" "$(basename "$1")" >>$output
+
+    for entry in "$1"/*; do
+        if [[ -f "$entry" ]]; then
+            filc=$(($filc + 1))
+            echo "$(
+                cd "$(dirname "$entry")"
+                pwd -P
+            )/$(basename "$entry")" >>$output
+        elif [[ -d "$entry" ]]; then
+            dirc=$(($dirc + 1))
+            echo "$(
+                cd "$(dirname "$entry")"
+                pwd -P
+            )/$(basename "$entry")" >>$output
+        fi
+    done
+    echo >>$output
+
+    for entry in "$1"/*; do
+        if [[ -d "$entry" ]]; then
+            walk "$entry"
+        fi
+    done
+}
+
+[ -e $output ] && rm $output # remove file.info if it already exists
+walk "$1"
+printf "[Directories Count]:%d\n" "$dirc" >>$output
+printf "[Files Count]:%d\n" "$filc" >>$output
